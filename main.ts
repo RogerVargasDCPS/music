d5909066a935cf97398b31c7b0f49a784836f677
RogerVargasDCPS <157537048+RogerVargasDCPS@users.noreply.github.com>
2024-01-23 16:01:20 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,4 +5,5 @@ input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Sad)
     music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     music.play(music.stringPlayable("A E G F D D C C ", 120), music.PlaybackMode.UntilDone)
+    basic.showIcon(IconNames.Happy)
 })
